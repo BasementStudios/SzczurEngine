2cccc9a0e6e501277db013e012e4b7451163ffdb
MetGang <patrick_stritch@wp.pl>
2017-11-02 16:49:36 +0100

    Separated Hash.hpp into .hpp and .tpp

--- src/Szczur/Utils/Hash.hpp
+++ src/Szczur/Utils/Hash.hpp
@@ -7,54 +7,17 @@ namespace rat {
 	using Hash64_t = uint64_t;
 
 	template<typename InputIt>
-	constexpr Hash32_t fnv1a_32(InputIt begin, InputIt end) {
-		Hash32_t value = 0x811C9DC5u;
-
-		while (begin != end) {
-			value ^= *begin;
-			value *= 0x1000193u;
-			++begin;
-		}
-
-		return value;
-	}
+	constexpr Hash32_t fnv1a_32(InputIt begin, InputIt end);
 
 	template<typename InputIt>
-	constexpr Hash32_t fnv1a_32(InputIt begin) {
-		Hash32_t value = 0x811C9DC5u;
-
-		while (*begin) {
-			value ^= *begin;
-			value *= 0x1000193u;
-			++begin;
-		}
-
-		return value;
-	}
+	constexpr Hash32_t fnv1a_32(InputIt begin);
 
 	template<typename InputIt>
-	constexpr Hash64_t fnv1a_64(InputIt begin, InputIt end) {
-		Hash64_t value = 0xCBF29CE484222325ull;
-
-		while (begin != end) {
-			value ^= *begin;
-			value *= 0x100000001B3ull;
-			++begin;
-		}
-
-		return value;
-	}
+	constexpr Hash64_t fnv1a_64(InputIt begin, InputIt end);
 
 	template<typename InputIt>
-	constexpr Hash64_t fnv1a_64(InputIt begin) {
-		Hash64_t value = 0xCBF29CE484222325ull;
+	constexpr Hash64_t fnv1a_64(InputIt begin);
 
-		while (*begin) {
-			value ^= *begin;
-			value *= 0x100000001B3ull;
-			++begin;
-		}
+#include "Hash.tpp"
 
-		return value;
-	}
 }
