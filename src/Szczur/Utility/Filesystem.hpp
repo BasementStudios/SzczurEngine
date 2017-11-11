53555c719eb8e0ccc81fd90bb0499e85ac3ae023
MetGang <patrick_stritch@wp.pl>
2017-11-11 15:16:26 +0100

    Added <filesystem> dispatcher

---
+++ src/Szczur/Utility/Filesystem.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#if __has_include(<filesystem>)
+#include <filesystem>
+namespace fs = std::filesystem;
+#elif __has_include(<experimental/filesystem>)
+#include <experimental/filesystem>
+namespace fs = std::experimental::filesystem;
+#elif __has_include(<boost/filesystem.hpp>)
+#include <boost/filesystem.hpp>
+namespace fs = boost::filesystem;
+#else
+#error No <filesystem> header is avaible
+#endif
