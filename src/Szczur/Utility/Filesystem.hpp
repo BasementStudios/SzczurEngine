ac6c915bd5c58eed9078ed3da439253ba12b5126
PsychoX <psychoxivi@gmail.com>
2017-11-12 00:53:02 +0100

    Changed name of the filesystem namespace of `fs` for `filesystem` to fit future C++ standards.
    The world of C++ safe again!
    
    Related:
    http://en.cppreference.com/w/cpp/filesystem
    http://en.cppreference.com/w/cpp/header/filesystem
    https://msdn.microsoft.com/en-us/library/hh874694.aspx

--- src/Szczur/Utility/Filesystem.hpp
+++ src/Szczur/Utility/Filesystem.hpp
@@ -2,13 +2,13 @@
 
 #if __has_include(<filesystem>)
 #include <filesystem>
-namespace fs = std::filesystem;
+namespace filesystem = std::filesystem;
 #elif __has_include(<experimental/filesystem>)
 #include <experimental/filesystem>
-namespace fs = std::experimental::filesystem;
+namespace filesystem = std::experimental::filesystem;
 #elif __has_include(<boost/filesystem.hpp>)
 #include <boost/filesystem.hpp>
-namespace fs = boost::filesystem;
+namespace filesystem = boost::filesystem;
 #else
 #error No <filesystem> header is avaible
 #endif
