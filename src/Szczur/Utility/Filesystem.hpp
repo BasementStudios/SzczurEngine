d1571d9dfa5322af028a41a21201f4236024cc1c
PsychoX <psychoxivi@gmail.com>
2017-11-12 01:18:02 +0100

    Included namespace `filesystem` to `std`.
    Consider that as a polyfill.

--- src/Szczur/Utility/Filesystem.hpp
+++ src/Szczur/Utility/Filesystem.hpp
@@ -1,14 +1,18 @@
 #pragma once
 
 #if __has_include(<filesystem>)
-#include <filesystem>
-namespace filesystem = std::filesystem;
+    #include <filesystem>
+    // Already should be okay.
 #elif __has_include(<experimental/filesystem>)
-#include <experimental/filesystem>
-namespace filesystem = std::experimental::filesystem;
+    #include <experimental/filesystem>
+    namespace std {
+        namespace filesystem = std::experimental::filesystem;
+    }
 #elif __has_include(<boost/filesystem.hpp>)
-#include <boost/filesystem.hpp>
-namespace filesystem = boost::filesystem;
+    #include <boost/filesystem.hpp>
+    namespace std {
+        namespace filesystem = boost::filesystem;
+    }
 #else
-#error No <filesystem> header is avaible
+    #error No <filesystem> header is avaible
 #endif
