0fa0c9896cc6ee7287fc7c1c353abbc3452c47cb
MetGang <patrick_stritch@wp.pl>
2017-11-22 12:08:05 +0100

    Added base for Lua module

---
+++ src/Szczur/Modules/Lua/Lua.cpp
@@ -0,0 +1,5 @@
+#include "Lua.hpp"
+
+namespace rat {
+
+}
---
+++ src/Szczur/Modules/Lua/Lua.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "Szczur/Utility/Lua.hpp"
+#include "Szczur/Utility/Modules.hpp"
+
+namespace rat {
+	class Lua : public ModuleBase<> { using ModuleBase::ModuleBase;
+
+	};
+}
