2d2a973e00136062749bf8e09414702dbc772bdb
MetGang <patrick_stritch@wp.pl>
2017-11-22 11:57:01 +0100

    Changed spaces into tabs

--- src/Szczur/Utility/Filesystem.hpp
+++ src/Szczur/Utility/Filesystem.hpp
@@ -1,18 +1,17 @@
 #pragma once
 
 #if __has_include(<filesystem>)
-    #include <filesystem>
-    // Already should be okay.
+	#include <filesystem>
 #elif __has_include(<experimental/filesystem>)
-    #include <experimental/filesystem>
-    namespace std {
-        namespace filesystem = std::experimental::filesystem;
-    }
+	#include <experimental/filesystem>
+	namespace std {
+		namespace filesystem = std::experimental::filesystem;
+	}
 #elif __has_include(<boost/filesystem.hpp>)
-    #include <boost/filesystem.hpp>
-    namespace std {
-        namespace filesystem = boost::filesystem;
-    }
+	#include <boost/filesystem.hpp>
+	namespace std {
+		namespace filesystem = boost::filesystem;
+	}
 #else
-    #error No <filesystem> header is avaible
+	#error No <filesystem> header is avaible
 #endif
