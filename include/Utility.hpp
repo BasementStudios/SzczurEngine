87e7a6426072262f079e95895f628cc30be292cc
Dubgron <mrdubgron@gmail.com>
2017-11-28 21:45:02 +0100

    Added utility that counts player Elements

--- include/Utility.hpp
+++ include/Utility.hpp
@@ -8,4 +8,13 @@ namespace rat
 	inline constexpr auto enum_v(T enum_var) {
 		return static_cast<std::underlying_type_t<T>>(enum_var);
 	}
+
+	template<typename T, size_t U>
+	constexpr unsigned int elements_c(std::array<T, U> container) {
+		unsigned int result = 0;
+		for (auto& i : container) {
+			result += i;
+		}
+		return result;
+	}
 }
