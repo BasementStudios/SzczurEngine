7f16cb0adb792ee5bba53aa2ea611380aeaf7358
Unknown <mapet1206@gmail.com>
2017-12-13 19:11:51 +0100

    Changed bracket style

--- src/Szczur/Modules/MusicMenager/MusicMenager.cpp
+++ src/Szczur/Modules/MusicMenager/MusicMenager.cpp
@@ -4,37 +4,44 @@
 
 namespace rat {
 
-	void MusicMenager::init() {
+	void MusicMenager::init() 
+	{
 
 	}
 
-	void MusicMenager::input(const sf::Event& event) {
+	void MusicMenager::input(const sf::Event& event) 
+	{
 
 	}
 
-	void MusicMenager::render() {
+	void MusicMenager::render() 
+	{
 
 	}
 
-	void MusicMenager::update(float deltaTime) {
+	void MusicMenager::update(float deltaTime) 
+	{
 		for(auto& it : _playlists) {
 			it->update(deltaTime);
 		}
 	}
 
 
-	void MusicMenager::push(std::vector<std::string> newPlaylist, int pos) {
+	void MusicMenager::push(std::vector<std::string> newPlaylist, int pos) 
+	{
 		if(pos < 0 || static_cast<unsigned>(pos) >= _playlists.size())
 			_playlists.push_back(std::make_shared<Playlist>(newPlaylist));
 		else
 			_playlists[pos] = std::make_shared<Playlist>(newPlaylist);
 	}
 
-	void MusicMenager::remove(int id) {
+	void MusicMenager::remove(int id) 
+	{
 		_playlists.erase(_playlists.begin() + id);
 	}
 
-	std::shared_ptr<Playlist> MusicMenager::operator[](int id) {
+	std::shared_ptr<Playlist> MusicMenager::operator[](int id) 
+	{
 		return _playlists[id];
 	}
 	
