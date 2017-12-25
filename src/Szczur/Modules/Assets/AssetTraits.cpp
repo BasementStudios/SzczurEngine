cf59ee21b4b5376728e00b8e7025a45627d734be
MetGang <patrick_stritch@wp.pl>
2017-12-25 18:16:10 +0100

    Splited AssetTraits into .hpp and .cpp

---
+++ src/Szczur/Modules/Assets/AssetTraits.cpp
@@ -0,0 +1,118 @@
+#include "AssetTraits.hpp"
+
+#include <fstream>
+#include <streambuf>
+
+#include <SFML/Graphics/Font.hpp>
+#include <SFML/Graphics/Texture.hpp>
+#include <SFML/Graphics/Shader.hpp>
+#include <SFML/Audio/Music.hpp>
+#include <SFML/Audio/SoundBuffer.hpp>
+#include <SFML/System/MemoryInputStream.hpp>
+
+namespace rat
+{
+
+sf::Font* AssetTraits<sf::Font>::create()
+{
+	return new sf::Font;
+}
+
+bool AssetTraits<sf::Font>::load(sf::Font& font, const std::string& path)
+{
+	return font.loadFromFile(path);
+}
+
+void AssetTraits<sf::Font>::unload(sf::Font& font)
+{
+	font.~Font();
+}
+
+sf::Texture* AssetTraits<sf::Texture>::create()
+{
+	return new sf::Texture;
+}
+
+bool AssetTraits<sf::Texture>::load(sf::Texture& texture, const std::string& path)
+{
+	return texture.loadFromFile(path);
+}
+
+void AssetTraits<sf::Texture>::unload(sf::Texture& texture)
+{
+	texture.~Texture();
+}
+
+sf::Shader* AssetTraits<sf::Shader>::create()
+{
+	return new sf::Shader;
+}
+
+bool AssetTraits<sf::Shader>::load(sf::Shader& shader, const std::string& path)
+{
+	std::ifstream file(path);
+
+	if(!file.good())
+		return false;
+
+	std::string buffer;
+
+	file.seekg(0, std::ios::end);
+	buffer.reserve(file.tellg());
+	file.seekg(0, std::ios::beg);
+
+	buffer.assign(std::istreambuf_iterator<char>(file), std::istreambuf_iterator<char>());
+
+	sf::MemoryInputStream streams[3];
+
+	auto it1 = std::begin(buffer);
+	auto it2 = std::find(it1, std::end(buffer), '$');
+	streams[0].open(it1.base(), std::distance(it1, it2));
+
+	it1 = std::next(it2);
+	it2 = std::find(it1, std::end(buffer), '$');
+	streams[1].open(it1.base(), std::distance(it1, it2));
+
+	it1 = std::next(it2);
+	it2 = std::find(it1, std::end(buffer), '$');
+	streams[2].open(it1.base(), std::distance(it1, it2));
+
+	return shader.loadFromStream(streams[0], streams[1], streams[2]);
+}
+
+void AssetTraits<sf::Shader>::unload(sf::Shader& shader)
+{
+	shader.~Shader();
+}
+
+sf::Music* AssetTraits<sf::Music>::create()
+{
+	return new sf::Music;
+}
+
+bool AssetTraits<sf::Music>::load(sf::Music& music, const std::string& path)
+{
+	return music.openFromFile(path);
+}
+
+void AssetTraits<sf::Music>::unload(sf::Music& music)
+{
+	music.~Music();
+}
+
+sf::SoundBuffer* AssetTraits<sf::SoundBuffer>::create()
+{
+	return new sf::SoundBuffer;
+}
+
+bool AssetTraits<sf::SoundBuffer>::load(sf::SoundBuffer& soundBuffer, const std::string& path)
+{
+	return soundBuffer.loadFromFile(path);
+}
+
+void AssetTraits<sf::SoundBuffer>::unload(sf::SoundBuffer& soundBuffer)
+{
+	soundBuffer.~SoundBuffer();
+}
+
+}
--- src/Szczur/Modules/Assets/AssetTraits.hpp
+++ src/Szczur/Modules/Assets/AssetTraits.hpp
@@ -1,14 +1,17 @@
 #pragma once
 
-#include <fstream>
-#include <streambuf>
+#include <string>
 
-#include <SFML/Graphics/Font.hpp>
-#include <SFML/Graphics/Texture.hpp>
-#include <SFML/Graphics/Shader.hpp>
-#include <SFML/Audio/Music.hpp>
-#include <SFML/Audio/SoundBuffer.hpp>
-#include <SFML/System/MemoryInputStream.hpp>
+namespace sf
+{
+
+class Font;
+class Texture;
+class Shader;
+class Music;
+class SoundBuffer;
+
+}
 
 namespace rat
 {
@@ -19,127 +22,55 @@ struct AssetTraits;
 template<>
 struct AssetTraits<sf::Font>
 {
-	static sf::Font* create()
-	{
-		return new sf::Font;
-	}
+	static sf::Font* create();
 
-	static bool load(sf::Font& font, const std::string& path)
-	{
-		return font.loadFromFile(path);
-	}
+	static bool load(sf::Font& font, const std::string& path);
 
-	static void unload(sf::Font& font)
-	{
-		font.~Font();
-	}
+	static void unload(sf::Font& font);
 
 };
 
 template<>
 struct AssetTraits<sf::Texture>
 {
-	static sf::Texture* create()
-	{
-		return new sf::Texture;
-	}
+	static sf::Texture* create();
 
-	static bool load(sf::Texture& texture, const std::string& path)
-	{
-		return texture.loadFromFile(path);
-	}
+	static bool load(sf::Texture& texture, const std::string& path);
 
-	static void unload(sf::Texture& texture)
-	{
-		texture.~Texture();
-	}
+	static void unload(sf::Texture& texture);
 
 };
 
 template<>
 struct AssetTraits<sf::Shader>
 {
-	static sf::Shader* create()
-	{
-		return new sf::Shader;
-	}
+	static sf::Shader* create();
 
-	static bool load(sf::Shader& shader, const std::string& path)
-	{
-		std::ifstream file(path);
+	static bool load(sf::Shader& shader, const std::string& path);
 
-		if(!file.good())
-			return false;
-
-		std::string buffer;
-
-		file.seekg(0, std::ios::end);
-		buffer.reserve(file.tellg());
-		file.seekg(0, std::ios::beg);
-
-		buffer.assign(std::istreambuf_iterator<char>(file), std::istreambuf_iterator<char>());
-
-		sf::MemoryInputStream streams[3];
-
-		auto it1 = std::begin(buffer);
-		auto it2 = std::find(it1, std::end(buffer), '$');
-		streams[0].open(it1.base(), std::distance(it1, it2));
-
-		it1 = std::next(it2);
-		it2 = std::find(it1, std::end(buffer), '$');
-		streams[1].open(it1.base(), std::distance(it1, it2));
-
-		it1 = std::next(it2);
-		it2 = std::find(it1, std::end(buffer), '$');
-		streams[2].open(it1.base(), std::distance(it1, it2));
-
-		return shader.loadFromStream(streams[0], streams[1], streams[2]);
-	}
-
-	static void unload(sf::Shader& shader)
-	{
-		shader.~Shader();
-	}
+	static void unload(sf::Shader& shader);
 
 };
 
 template<>
 struct AssetTraits<sf::Music>
 {
-	static sf::Music* create()
-	{
-		return new sf::Music;
-	}
+	static sf::Music* create();
 
-	static bool load(sf::Music& music, const std::string& path)
-	{
-		return music.openFromFile(path);
-	}
+	static bool load(sf::Music& music, const std::string& path);
 
-	static void unload(sf::Music& music)
-	{
-		music.~Music();
-	}
+	static void unload(sf::Music& music);
 
 };
 
 template<>
 struct AssetTraits<sf::SoundBuffer>
 {
-	static sf::SoundBuffer* create()
-	{
-		return new sf::SoundBuffer;
-	}
+	static sf::SoundBuffer* create();
 
-	static bool load(sf::SoundBuffer& soundBuffer, const std::string& path)
-	{
-		return soundBuffer.loadFromFile(path);
-	}
+	static bool load(sf::SoundBuffer& soundBuffer, const std::string& path);
 
-	static void unload(sf::SoundBuffer& soundBuffer)
-	{
-		soundBuffer.~SoundBuffer();
-	}
+	static void unload(sf::SoundBuffer& soundBuffer);
 
 };
 
