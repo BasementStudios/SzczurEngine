514c76c98b3f965b38b254b7fd187bae86ede9a9
Piotr Krupa <piotrkrupa06@gmail.com>
2017-12-31 14:12:37 +0100

    Changed brackets style

--- src/Szczur/Modules/DragonBones/DragonBones.cpp
+++ src/Szczur/Modules/DragonBones/DragonBones.cpp
@@ -1,19 +1,24 @@
 #include "DragonBones.hpp"
 
-namespace rat {
-	void DragonBones::init() {
+namespace rat 
+{
+	void DragonBones::init()
+	{
 		_factory = std::make_unique<dragonBones::SFMLFactory>();
 	}
 
-	void DragonBones::update(float deltaTime) {
+	void DragonBones::update(float deltaTime)
+	{
 		_factory->update(deltaTime);
 	}
 
-	void DragonBones::render() {
+	void DragonBones::render()
+	{
 
 	}
 
-	void DragonBones::input(sf::Event& e) {
+	void DragonBones::input(sf::Event& e)
+	{
 		(void)e;
 	}
 }
