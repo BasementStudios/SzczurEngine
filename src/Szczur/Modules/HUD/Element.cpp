9d34f64a1dea7620ad552300d3efbbe0738e97ee
Knayder <knayder@outlook.com>
2018-01-04 23:27:19 +0100

    Added smooth movement

--- src/Szczur/Modules/HUD/Element.cpp
+++ src/Szczur/Modules/HUD/Element.cpp
@@ -2,35 +2,74 @@
 
 namespace rat {
     Element::Element(const std::string& file, float radius, sf::Color color) :
-    _radius(radius),
-    _isReady(false),
+    _targetRadius(radius),
+    _followRadius(true),
+    _followPosition(false),
     _background(0.f) {
         _texture.loadFromFile(file);
         _icon.setTexture(_texture);
-        float k = (radius*1.5f) / _texture.getSize().y;
-        _icon.scale(k,k);
-
+        _icon.setScale(0,0);
         _background.setOrigin(radius, radius);
         _background.setFillColor(color);
         _icon.setOrigin(_texture.getSize().x/2, _texture.getSize().y/2);
     }
 
     void Element::update(float deltaTime) {
-        if(!_isReady) {
-            _background.setRadius(_background.getRadius() + (155.f*deltaTime));
-            if(_background.getRadius() >= _radius) {
-                _background.setRadius(_radius);
-                _isReady = true;
-            }
-            _background.setOrigin(_background.getRadius(), _background.getRadius());
-        }
+        if(_followRadius)
+            _followTargetRadius(deltaTime);
+
+        if(_followPosition)
+            _followTargetPosition(deltaTime);
     }
 
     void Element::draw(sf::RenderTarget& target, sf::RenderStates states) const {
         states.transform *= getTransform();
 
         target.draw(_background, states);
-        if(_isReady)
-            target.draw(_icon, states);
+        target.draw(_icon, states);
+    }
+
+    void Element::_followTargetRadius(float deltaTime) {
+        float curR = _background.getRadius();
+        curR = curR + (
+            (
+                _targetRadius - curR
+            )/(5000.f*deltaTime)
+        );
+        _background.setRadius(curR);
+        float k = (curR*1.5f) / _texture.getSize().y;
+        _icon.setScale(k,k);
+        if(std::abs( curR - _targetRadius ) < 0.1f) {
+            _background.setRadius(_targetRadius);
+            _followRadius = false;
+            _ready = true;
+        }
+        curR = _background.getRadius();
+        _background.setOrigin(curR, curR);
+    }
+
+    void Element::_followTargetPosition(float deltaTime) {
+        move( 
+            (
+                _targetPosition - getPosition()
+            ) / (10000.f*deltaTime) 
+        );
+        sf::Vector2f delta = _targetPosition - getPosition();
+        delta.x = std::abs(delta.x);
+        delta.y = std::abs(delta.y);
+        if(delta.x < 0.1f && delta.y < 0.1f) {
+            setPosition(_targetPosition);
+            _followPosition = false;
+        }
+    }
+
+    void Element::setTargetRadius(float value) {
+        _targetRadius = value;
+        _followRadius = true;
+    }
+
+    void Element::setTargetPosition(sf::Vector2f value) {
+        _targetPosition = value;
+        _followPosition = true;
     }
 }
--- src/Szczur/Modules/HUD/Element.hpp
+++ src/Szczur/Modules/HUD/Element.hpp
@@ -7,6 +7,9 @@ namespace rat {
     public:
         Element(const std::string& file, float radius, sf::Color color);
 
+        void setTargetRadius(float value);
+        void setTargetPosition(sf::Vector2f value);
+
         void update(float deltaTime);
     private:
         sf::CircleShape _background;
@@ -14,8 +17,14 @@ namespace rat {
 
         sf::Texture _texture;
 
-        bool _isReady;
-        float _radius;
+        bool _followPosition;
+        bool _followRadius;
+        bool _ready;
+        float _targetRadius;
+        sf::Vector2f _targetPosition;
+
+        void _followTargetRadius(float deltaTime);
+        void _followTargetPosition(float deltaTime);
 
         virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
     };
