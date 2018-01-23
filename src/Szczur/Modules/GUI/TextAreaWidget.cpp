2f614e7fa06346d2a1d52819aad28513334d11a6
Knayder <knayder@outlook.com>
2018-01-23 14:28:12 +0100

    Added TextArea class

---
+++ src/Szczur/Modules/GUI/TextAreaWidget.cpp
@@ -0,0 +1,105 @@
+#include "TextAreaWidget.hpp"
+#include <iostream>
+
+namespace rat {
+    TextAreaWidget::TextAreaWidget() :
+    _size(0u, 0u),
+    _toUpdate(false),
+    _toCreate(false),
+    _toWrap(false) {
+        _view = _area.getView();
+        _sprite.setTexture(_area.getTexture());
+    }
+
+    TextAreaWidget::TextAreaWidget(sf::Vector2u size, sf::Font* font) :
+    _size(size),
+    _toUpdate(false),
+    _toCreate(false),
+    _toWrap(false) {
+        _area.create(size.x * _text.getCharacterSize() * 0.6, size.y * _text.getCharacterSize() * 1.2);
+        _view = _area.getView();
+
+        _text.setFont(*font);
+        _sprite.setTexture(_area.getTexture());
+    }
+
+    void TextAreaWidget::setString(std::string text) {
+        _text.setString(text);
+        _toCreate = true;
+        _toWrap = true;
+    }
+
+    void TextAreaWidget::setSize(sf::Vector2u size) {
+        _size = size;
+        _toCreate = true;
+    }
+
+    void TextAreaWidget::setFont(sf::Font* font) {
+        _text.setFont(*font);
+        _toCreate = true;
+    }
+
+    void TextAreaWidget::setCharacterSize(size_t size) {
+        _text.setCharacterSize(size);
+        _toCreate = true;
+    }
+
+    void TextAreaWidget::setColor(const sf::Color& color) {
+        _text.setFillColor(color);
+        _toCreate = true;
+    }
+
+    void TextAreaWidget::_draw(sf::RenderTarget& target, sf::RenderStates states) const {
+        sf::Sprite sprite;
+        sprite.setTexture(_area.getTexture());
+
+        //std::cout << reinterpret_cast<const void*>(sprite.getTexture()) << "  " << reinterpret_cast<const void*>(_sprite.getTexture()) << '\n';
+
+        target.draw(sprite, states);
+    }
+
+    void TextAreaWidget::_redrawArea() {
+        _area.clear(sf::Color::Transparent);
+        _area.draw(_text);
+        _area.display();
+    }
+
+    void TextAreaWidget::_wrapText() {
+        std::cout << "Wrap\n";
+        std::string temp = _text.getString().toAnsiString();
+        temp.erase(std::remove_if(temp.begin(), temp.end(), [](auto& it){return it=='\n';}), temp.end());
+        for(auto i = _size.x; i<temp.size(); i+=_size.x)
+            temp.insert(i, 1, '\n');
+        _text.setString(temp);
+    }
+
+    void TextAreaWidget::_update(float deltaTime) {
+        if(_toWrap) {
+            _toWrap = false;
+            _wrapText();
+        }
+        if(_toCreate) {
+            _toCreate = false;
+            _area.create(_size.x * _text.getCharacterSize() * 0.6, _size.y * _text.getCharacterSize() * 1.2);
+            _redrawArea();
+            _view = _area.getView();
+            _aboutToRecalculate = true;
+        }
+        if(_toUpdate) {
+            _toUpdate = false;
+            _area.setView(_view);
+            _redrawArea();
+        }
+    }
+
+    void TextAreaWidget::_input(const sf::Event& event) {
+        if(_isHovered && event.type == sf::Event::MouseWheelScrolled) {
+            _view.move({0.f, -10.f*static_cast<float>(event.mouseWheelScroll.delta)});
+            _toUpdate = true;
+        }
+    }
+
+    sf::Vector2u TextAreaWidget::_getSize() const {
+        return _area.getSize();
+    }
+}
---
+++ src/Szczur/Modules/GUI/TextAreaWidget.hpp
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+
+#include <algorithm>
+
+#include "TextWidget.hpp"
+
+namespace rat {
+    class TextAreaWidget : public Widget {
+    public:
+        TextAreaWidget();
+        TextAreaWidget(sf::Vector2u size, sf::Font* font);
+
+        void setString(std::string text);
+
+        void setSize(sf::Vector2u size);
+
+        void setFont(sf::Font* font);
+
+        void setCharacterSize(size_t size);
+
+        void setColor(const sf::Color& color);
+
+    protected:
+        virtual void _update(float deltaTime) override;
+        virtual void _draw(sf::RenderTarget& target, sf::RenderStates states) const override;
+        virtual void _input(const sf::Event& event) override;
+        virtual sf::Vector2u _getSize() const override;
+    private:
+        bool _toCreate;
+        bool _toUpdate;
+        bool _toWrap;
+
+        sf::Text _text;
+        sf::RenderTexture _area;
+        sf::Sprite _sprite;
+        sf::Vector2u _size;
+        sf::View _view;
+
+        void _redrawArea();
+        void _wrapText();
+    };
+}
