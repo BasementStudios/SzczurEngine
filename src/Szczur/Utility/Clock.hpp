5fd774110d372700ee1148311090f7ac3d9688d4
MetGang <patrick_stritch@wp.pl>
2018-01-25 23:48:55 +0100

    Splited time classes into .hpp, .tpp and .cpp

--- src/Szczur/Utility/Clock.hpp
+++ src/Szczur/Utility/Clock.hpp
@@ -9,59 +9,34 @@ class Clock
 {
 public:
 
-    using Clock_t     = Time::Clock_t;
-    using Duration_t  = Time::Duration_t;
-    using TimePoint_t = Time::TimePoint_t;
-    using Period_t    = Time::Period_t;
+	using Clock_t     = Time::Clock_t;
+	using Duration_t  = Time::Duration_t;
+	using TimePoint_t = Time::TimePoint_t;
+	using Period_t    = Time::Period_t;
 
-    static TimePoint_t now()
-    {
-        return Clock_t::now();
-    }
+	static TimePoint_t now();
 
-    Clock() :
-        _refPoint(now())
-    {
+	Clock();
 
-    }
+	Clock(const Clock&) = default;
 
-    Clock(const Clock&) = default;
+	Clock& operator = (const Clock&) = default;
 
-    Clock& operator = (const Clock&) = default;
+	Clock(Clock&&) = default;
 
-    Clock(Clock&&) = default;
+	Clock& operator = (Clock&&) = default;
 
-    Clock& operator = (Clock&&) = default;
+	Time getElapsedTime() const;
 
-    Time getElapsedTime() const
-    {
-        return now() - _refPoint;
-    }
+	Time restart();
 
-    Time restart()
-    {
-        Time ret = getElapsedTime();
-        _refPoint = now();
-        return ret;
-    }
+	bool hasPassed(const Duration_t& duration) const;
 
-    bool hasPassed(const Duration_t& duration) const
-    {
-        return duration >= getElapsedTime().getDuration();
-    }
-
-    bool checkStep(const Duration_t& duration)
-    {
-        if(hasPassed(duration)) {
-            restart();
-            return true;
-        }
-        return false;
-    }
+	bool checkStep(const Duration_t& duration);
 
 private:
 
-    TimePoint_t _refPoint;
+	TimePoint_t _refPoint;
 
 };
 
--- src/Szczur/Utility/Timer.hpp
+++ src/Szczur/Utility/Timer.hpp
@@ -9,85 +9,43 @@ class Timer
 {
 public:
 
-    using Clock_t     = Time::Clock_t;
-    using Duration_t  = Time::Duration_t;
-    using TimePoint_t = Time::TimePoint_t;
-    using Period_t    = Time::Period_t;
+	using Clock_t     = Time::Clock_t;
+	using Duration_t  = Time::Duration_t;
+	using TimePoint_t = Time::TimePoint_t;
+	using Period_t    = Time::Period_t;
 
-    static TimePoint_t now()
-    {
-        return Clock_t::now();
-    }
+	static TimePoint_t now();
 
-    Timer() :
-        _refPoint(now()), _pausePoint(now())
-    {
+	Timer();
 
-    }
+	Timer(const Timer&) = default;
 
-    Timer(const Timer&) = default;
+	Timer& operator = (const Timer&) = default;
 
-    Timer& operator = (const Timer&) = default;
+	Timer(Timer&&) = default;
 
-    Timer(Timer&&) = default;
+	Timer& operator = (Timer&&) = default;
 
-    Timer& operator = (Timer&&) = default;
+	Time getElapsedTime() const;
 
-    Time getElapsedTime() const
-    {
-        return isPaused() ? _pausePoint - _refPoint : now() - _refPoint;
-    }
+	void start();
 
-    void start()
-    {
-        _refPoint += now() - _pausePoint;
-        _pausePoint = TimePoint_t(Duration_t::zero());
-    }
+	void pause();
 
-    void pause()
-    {
-        _pausePoint = now();
-    }
+	void toggle();
 
-    void toggle()
-    {
-        if(isPaused())
-            start();
-        else
-            pause();
-    }
+	Time restart();
 
-    Time restart()
-    {
-        Time Ret = getElapsedTime();
-        _pausePoint = TimePoint_t(Duration_t::zero());
-        _refPoint = now();
-        return Ret;
-    }
+	bool isPaused() const;
 
-    bool isPaused() const
-    {
-        return _pausePoint.time_since_epoch() != Duration_t::zero();
-    }
+	bool hasPassed(const Duration_t& duration) const;
 
-    bool hasPassed(const Duration_t& duration) const
-    {
-        return duration >= getElapsedTime().getDuration();
-    }
-
-    bool checkStep(const Duration_t& duration)
-    {
-        if(hasPassed(duration)) {
-            restart();
-            return true;
-        }
-        return false;
-    }
+	bool checkStep(const Duration_t& duration);
 
 private:
 
-    TimePoint_t _refPoint;
-    TimePoint_t _pausePoint;
+	TimePoint_t _refPoint;
+	TimePoint_t _pausePoint;
 
 };
 
