9758127dc40c37d205a3ef8699ca50fabc74be9f
MetGang <patrick_stritch@wp.pl>
2018-01-29 01:43:35 +0100

    Removed unnecessary brackets

--- src/Szczur/Debug/Logger.hpp
+++ src/Szczur/Debug/Logger.hpp
@@ -53,12 +53,12 @@ inline DebugLogger* logger;
 #define LOG_INFO(...) { rat::logger->log(__FILE__, __LINE__, "[INFO] ", __VA_ARGS__); }
 #define LOG_WARN(...) { rat::logger->log(__FILE__, __LINE__, "[WARN] ", __VA_ARGS__); }
 #define LOG_ERROR(...) { rat::logger->log(__FILE__, __LINE__, "[ERROR] ", __VA_ARGS__); }
-#define LOG_INFO_IF(condition, ...) { if(condition) { LOG_INFO(__VA_ARGS__) } }
-#define LOG_WARN_IF(condition, ...) { if(condition) { LOG_WARN(__VA_ARGS__) } }
-#define LOG_ERROR_IF(condition, ...) { if(condition) { LOG_ERROR(__VA_ARGS__) } }
-#define LOG_INFO_IF_CX(condition, ...) { if constexpr(condition) { LOG_INFO(__VA_ARGS__) } }
-#define LOG_WARN_IF_CX(condition, ...) { if constexpr(condition) { LOG_WARN(__VA_ARGS__) } }
-#define LOG_ERROR_IF_CX(condition, ...) { if constexpr(condition) { LOG_ERROR(__VA_ARGS__) } }
+#define LOG_INFO_IF(condition, ...) { if(condition) LOG_INFO(__VA_ARGS__) }
+#define LOG_WARN_IF(condition, ...) { if(condition) LOG_WARN(__VA_ARGS__) }
+#define LOG_ERROR_IF(condition, ...) { if(condition) LOG_ERROR(__VA_ARGS__) }
+#define LOG_INFO_IF_CX(condition, ...) { if constexpr(condition) LOG_INFO(__VA_ARGS__) }
+#define LOG_WARN_IF_CX(condition, ...) { if constexpr(condition) LOG_WARN(__VA_ARGS__) }
+#define LOG_ERROR_IF_CX(condition, ...) { if constexpr(condition) LOG_ERROR(__VA_ARGS__) }
 
 #else
 
