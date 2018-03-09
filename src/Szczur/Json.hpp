74cd3c40cfce6b3a0d16abe575791417c5fad7e2
PsychoXIVI <psychoxivi@gmail.com>
2018-03-09 23:25:15 +0100

    Renamed json object type to fit convention

--- src/Szczur/Json.hpp
+++ src/Szczur/Json.hpp
@@ -2,10 +2,10 @@
 
 #if __has_include(<Json/json.hpp>)
 #	include <Json/json.hpp>
-	using Json = nlohmann::json;
+	using json = nlohmann::json;
 #elif __has_include(<json.hpp>)
 #	include <json.hpp>
-	using Json = nlohmann::json;
+	using json = nlohmann::json;
 #else
 #	error No <json.hpp> header is avaible
 #endif
