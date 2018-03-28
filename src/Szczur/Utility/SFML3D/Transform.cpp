becb8bc5d12185440bd30b7d360082925d6eed7b
Knayder <knayder@outlook.com>
2018-03-29 00:30:49 +0200

    Coded Transform class

--- src/Szczur/Utility/SFML3D/Transform.cpp
+++ src/Szczur/Utility/SFML3D/Transform.cpp
@@ -1,5 +1,24 @@
 #include "Transform.hpp"
 
 namespace rat::sf3d {
-    
+    Transform::Transform():
+    _mat(1.f) {
+
+    }
+
+    void Transform::translate(const glm::vec3& offset) {
+        _mat = glm::translate(_mat, offset);
+    }
+
+    void Transform::rotate(float angle, const glm::vec3& direction) {
+        _mat = glm::rotate(_mat, angle, direction);
+    }
+
+    void Transform::scale(const glm::vec3& offset) {
+        _mat = glm::scale(_mat, offset);
+    }
+
+    void Transform::combine(const Transform& transform) {
+        _mat *= transform;
+    }
 }
--- src/Szczur/Utility/SFML3D/Transform.hpp
+++ src/Szczur/Utility/SFML3D/Transform.hpp
@@ -1,7 +1,26 @@
 #pragma once
 
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
 namespace rat::sf3d {
     class Transform {
+    public:
+        Transform();
 
+        void translate(const glm::vec3& offset);
+
+        void rotate(float angle, const glm::vec3& direction);
+
+        void scale(const glm::vec3& offset);
+
+        void combine(const Transform& transform);
+
+        operator glm::mat4() {
+            return _mat;
+        }
+
+    //private:
+        glm::mat4 _mat;
     };
 }
