5aa578337c0efe660f659adfb2f9e74e20428415
Piotr Krupa <piotr.krupa.priv@gmail.com>
2018-04-09 17:36:17 +0200

    Added autocomplete file extension to file name

--- src/Szczur/Modules/FileSystem/FileDialog.cpp
+++ src/Szczur/Modules/FileSystem/FileDialog.cpp
@@ -93,7 +93,8 @@ bool FileDialog::show(char *result, bool multiselect)
 	ofn.lpstrTitle = _caption.c_str();
 	ofn.lpstrInitialDir = _directory.c_str();
 	ofn.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST | (multiselect ? OFN_ALLOWMULTISELECT | OFN_EXPLORER : 0x0);
-
+	ofn.lpstrDefExt = "rle";
+	
 	bool accepted = false;
 
 	if (_mode == Open)
