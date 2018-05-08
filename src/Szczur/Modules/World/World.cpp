d3897dcb1a63939f8fb7ffee088e5f0e2c976b7c
Knayder <knayder@outlook.com>
2018-05-08 14:53:33 +0200

    Created Scene Manager

--- src/Szczur/Modules/World/World.cpp
+++ src/Szczur/Modules/World/World.cpp
@@ -4,7 +4,6 @@ namespace rat
 {
 
 World::World()
-	: _currentSceneID { 0 }
 {
 	LOG_INFO("Initializing World module");
 
@@ -18,142 +17,23 @@ World::~World()
 
 void World::update(float deltaTime)
 {
-	if (isCurrentSceneValid())
+	if (_scenes.isCurrentSceneValid())
 	{
-		getCurrentScene()->update(deltaTime);
+		_scenes.getCurrentScene()->update(deltaTime);
 	}
 }
 
 void World::render()
 {
-	if (isCurrentSceneValid())
+	if (_scenes.isCurrentSceneValid())
 	{
 		auto& window = getModule<Window>();
-		getCurrentScene()->forEach([&window](const std::string&, Entity& entity) { 
+		_scenes.getCurrentScene()->forEach([&window](const std::string&, Entity& entity) { 
 			if (auto ptr = entity.getFeature<sf3d::Drawable>(); ptr != nullptr) { 
 				window.draw(*ptr); 
 			} 
 		}); 
 	}
-}
-
-Scene* World::addScene()
-{
-	return _holder.emplace_back(std::make_unique<Scene>()).get();
-}
-
-bool World::removeScene(size_t id)
-{
-	if (auto it = _find(id); it != _holder.end())
-	{
-		_holder.erase(it);
-
-		if (_currentSceneID == id)
-		{
-			_currentSceneID = 0;
-		}
-
-		return true;
-	}
-
-	return false;
-}
-
-void World::removeAllScenes()
-{
-	_holder.clear();
-
-	_currentSceneID = 0;
-}
-
-Scene* World::getScene(size_t id) const
-{
-	if (auto it = _find(id); it != _holder.end())
-	{
-		return it->get();
-	}
-
-	return nullptr;
-}
-
-Scene* World::getCurrentScene() const
-{
-	return getScene(_currentSceneID);
-}
-
-size_t World::getCurrentSceneID() const
-{
-	return _currentSceneID;
-}
-
-bool World::hasScene(size_t id) const
-{
-	return _find(id) != _holder.end();
-}
-
-bool World::isCurrentSceneValid() const
-{
-	return _currentSceneID != 0;
-}
-
-World::ScenesHolder_t& World::getScenes()
-{
-	return _holder;
-}
-
-const World::ScenesHolder_t& World::getScenes() const
-{
-	return _holder;
-}
-
-void World::loadFromFile(const std::string& filepath)
-{
-	std::ifstream file{ filepath };
-	Json config;
-
-	file >> config;
-
-	_currentSceneID = config["currentSceneID"];
-
-	const Json& scenes = config["scenes"];
-
-	for (auto& current : scenes)
-	{
-		addScene()->loadFromConfig(current);
-	}
-}
-
-void World::saveToFile(const std::string& filepath) const
-{
-	std::ofstream file{ filepath };
-	Json config;
-
-	config["currentSceneID"] = getCurrentSceneID();
-	Json& scenes = config["scenes"] = Json::array();
-
-	for (auto& scene : _holder)
-	{
-		scenes.push_back(Json::object());
-		Json& current = scenes.back();
-
-		scene->saveToConfig(current);
-	}
-
-	file << std::setw(4) << config << std::endl;
-}
-
-typename World::ScenesHolder_t::iterator World::_find(size_t id)
-{
-	return std::find_if(_holder.begin(), _holder.end(), [=](const auto& arg) {
-		return arg->getID() == id;
-	});
-}
-
-typename World::ScenesHolder_t::const_iterator World::_find(size_t id) const
-{
-	return std::find_if(_holder.begin(), _holder.end(), [=](const auto& arg) {
-		return arg->getID() == id;
-	});
 }
 
 }
--- src/Szczur/Modules/World/World.hpp
+++ src/Szczur/Modules/World/World.hpp
@@ -7,15 +7,14 @@
 #include "Szczur/Modules/Input/Input.hpp"
 #include "Szczur/Utility/Modules/Module.hpp"
 
+#include "SceneManager.hpp"
+
 namespace rat
 {
 
 class World : public Module<Window, Input>
 {
 public:
-
-	using ScenesHolder_t = std::vector<std::unique_ptr<Scene>>;
-
 	///
 	World();
 
@@ -40,53 +39,8 @@ public:
 	///
 	void render();
 
-	///
-	Scene* addScene();
-
-	///
-	bool removeScene(size_t id);
-
-	///
-	void removeAllScenes();
-
-	///
-	Scene* getScene(size_t id) const;
-
-	///
-	Scene* getCurrentScene() const;
-
-	///
-	size_t getCurrentSceneID() const;
-
-	///
-	bool hasScene(size_t id) const;
-
-	///
-	bool isCurrentSceneValid() const;
-
-	///
-	ScenesHolder_t& getScenes();
-
-	///
-	const ScenesHolder_t& getScenes() const;
-
-	///
-	void loadFromFile(const std::string& filepath);
-
-	///
-	void saveToFile(const std::string& filepath) const;
-
 private:
-
-	///
-	typename ScenesHolder_t::iterator _find(size_t id);
-
-	///
-	typename ScenesHolder_t::const_iterator _find(size_t id) const;
-
-	ScenesHolder_t _holder;
-	size_t _currentSceneID;
-
+	SceneManager _scenes;
 };
 
 }
