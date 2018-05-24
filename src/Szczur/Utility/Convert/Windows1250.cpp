6b2f36cc07ec1e790b113209bb7fa1aaa6a08379
MetGang <patrick_stritch@wp.pl>
2018-05-24 19:43:01 +0200

    Surpress overflow warnings in cross-compiler way

--- src/Szczur/Utility/Convert/Windows1250.cpp
+++ src/Szczur/Utility/Convert/Windows1250.cpp
@@ -1,8 +1,5 @@
 #include "Windows1250.hpp"
 
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Woverflow"
-
 namespace rat
 {
 
@@ -21,24 +18,24 @@ std::string mapWindows1250ToUtf8(const std::string& src)
         {
             switch (static_cast<unsigned>(src[v]))
             {
-                case 0xA5: out += 0xC4; out += 0x84; break; // Ą
-                case 0xB9: out += 0xC4; out += 0x85; break; // ą
-                case 0xC6: out += 0xC4; out += 0x86; break; // Ć
-                case 0xE6: out += 0xC4; out += 0x87; break; // ć
-                case 0xCA: out += 0xC4; out += 0x98; break; // Ę
-                case 0xEA: out += 0xC4; out += 0x99; break; // ę
-                case 0xA3: out += 0xC5; out += 0x81; break; // Ł
-                case 0xB3: out += 0xC5; out += 0x82; break; // ł
-                case 0xD1: out += 0xC5; out += 0x83; break; // Ń
-                case 0xF1: out += 0xC5; out += 0x84; break; // ń
-                case 0xD3: out += 0xC3; out += 0x93; break; // Ó
-                case 0xF3: out += 0xC3; out += 0xB3; break; // ó
-                case 0x8C: out += 0xC5; out += 0x9A; break; // Ś
-                case 0x9C: out += 0xC5; out += 0x9B; break; // ś
-                case 0x8F: out += 0xC5; out += 0xB9; break; // Ź
-                case 0x9F: out += 0xC5; out += 0xBA; break; // ź
-                case 0xAF: out += 0xC5; out += 0xBB; break; // Ż
-                case 0xBF: out += 0xC5; out += 0xBC; break; // ż
+                case 0xA5: out += static_cast<char>(0xC4); out += static_cast<char>(0x84); break; // Ą
+                case 0xB9: out += static_cast<char>(0xC4); out += static_cast<char>(0x85); break; // ą
+                case 0xC6: out += static_cast<char>(0xC4); out += static_cast<char>(0x86); break; // Ć
+                case 0xE6: out += static_cast<char>(0xC4); out += static_cast<char>(0x87); break; // ć
+                case 0xCA: out += static_cast<char>(0xC4); out += static_cast<char>(0x98); break; // Ę
+                case 0xEA: out += static_cast<char>(0xC4); out += static_cast<char>(0x99); break; // ę
+                case 0xA3: out += static_cast<char>(0xC5); out += static_cast<char>(0x81); break; // Ł
+                case 0xB3: out += static_cast<char>(0xC5); out += static_cast<char>(0x82); break; // ł
+                case 0xD1: out += static_cast<char>(0xC5); out += static_cast<char>(0x83); break; // Ń
+                case 0xF1: out += static_cast<char>(0xC5); out += static_cast<char>(0x84); break; // ń
+                case 0xD3: out += static_cast<char>(0xC3); out += static_cast<char>(0x93); break; // Ó
+                case 0xF3: out += static_cast<char>(0xC3); out += static_cast<char>(0xB3); break; // ó
+                case 0x8C: out += static_cast<char>(0xC5); out += static_cast<char>(0x9A); break; // Ś
+                case 0x9C: out += static_cast<char>(0xC5); out += static_cast<char>(0x9B); break; // ś
+                case 0x8F: out += static_cast<char>(0xC5); out += static_cast<char>(0xB9); break; // Ź
+                case 0x9F: out += static_cast<char>(0xC5); out += static_cast<char>(0xBA); break; // ź
+                case 0xAF: out += static_cast<char>(0xC5); out += static_cast<char>(0xBB); break; // Ż
+                case 0xBF: out += static_cast<char>(0xC5); out += static_cast<char>(0xBC); break; // ż
                 default: break;
             }
         }
@@ -68,34 +65,34 @@ std::string mapUtf8ToWindows1250(const std::string& src)
             {
                 case 0xC3: switch (static_cast<unsigned>(src[v + 1u]))
                 {
-                    case 0x93: out += 0xD3; break; // Ó
-                    case 0xB3: out += 0xF3; break; // ó
+                    case 0x93: out += static_cast<char>(0xD3); break; // Ó
+                    case 0xB3: out += static_cast<char>(0xF3); break; // ó
                 }
                 break;
 
                 case 0xC4: switch (static_cast<unsigned>(src[v + 1u]))
                 {
-                    case 0x84: out += 0xA5; break; // Ą
-                    case 0x85: out += 0xB9; break; // ą
-                    case 0x86: out += 0xC6; break; // Ć
-                    case 0x87: out += 0xE6; break; // ć
-                    case 0x98: out += 0xCA; break; // Ę
-                    case 0x99: out += 0xEA; break; // ę
+                    case 0x84: out += static_cast<char>(0xA5); break; // Ą
+                    case 0x85: out += static_cast<char>(0xB9); break; // ą
+                    case 0x86: out += static_cast<char>(0xC6); break; // Ć
+                    case 0x87: out += static_cast<char>(0xE6); break; // ć
+                    case 0x98: out += static_cast<char>(0xCA); break; // Ę
+                    case 0x99: out += static_cast<char>(0xEA); break; // ę
                 }
                 break;
 
                 case 0xC5: switch (static_cast<unsigned>(src[v + 1u]))
                 {
-                    case 0x81: out += 0xA3; break; // Ł
-                    case 0x82: out += 0xB3; break; // ł
-                    case 0x83: out += 0xD1; break; // Ń
-                    case 0x84: out += 0xF1; break; // ń
-                    case 0x9A: out += 0x8C; break; // Ś
-                    case 0x9B: out += 0x9C; break; // ś
-                    case 0xB9: out += 0x8F; break; // Ź
-                    case 0xBA: out += 0x9F; break; // ź
-                    case 0xBB: out += 0xAF; break; // Ż
-                    case 0xBC: out += 0xBF; break; // ż
+                    case 0x81: out += static_cast<char>(0xA3); break; // Ł
+                    case 0x82: out += static_cast<char>(0xB3); break; // ł
+                    case 0x83: out += static_cast<char>(0xD1); break; // Ń
+                    case 0x84: out += static_cast<char>(0xF1); break; // ń
+                    case 0x9A: out += static_cast<char>(0x8C); break; // Ś
+                    case 0x9B: out += static_cast<char>(0x9C); break; // ś
+                    case 0xB9: out += static_cast<char>(0x8F); break; // Ź
+                    case 0xBA: out += static_cast<char>(0x9F); break; // ź
+                    case 0xBB: out += static_cast<char>(0xAF); break; // Ż
+                    case 0xBC: out += static_cast<char>(0xBF); break; // ż
                 }
                 break;
 
@@ -114,5 +111,3 @@ std::string mapUtf8ToWindows1250(const std::string& src)
 }
 
 }
-
-#pragma GCC diagnostic pop
