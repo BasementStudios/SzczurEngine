75440ceb40f088305c77a15db3199d9713da0f66
MetGang <patrick_stritch@wp.pl>
2018-05-24 19:59:07 +0200

    Lightened config header

--- src/Szczur/Config.hpp
+++ src/Szczur/Config.hpp
@@ -46,10 +46,7 @@
 // Editor specification
 #if defined(DEBUG) && defined(OS_WINDOWS)
 #	define EDITOR
-#	include <ImGui/imgui.h>
-#	include <ImGui/imgui-SFML.h>
-#	include "Szczur/Utility/Debug/GlobalVarsHolder.hpp"
 #endif
 
-// Global helper for modules system
+// Global helper ptr
 namespace rat::detail { template <typename T> inline T* globalPtr = nullptr; }
