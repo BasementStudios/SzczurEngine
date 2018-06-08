528c49dc731a59b5a489dc72190d963cd94d9dc6
Piotr Krupa <piotrkrupa06@gmail.com>
2018-06-08 21:58:28 +0200

    Added new moving script for player

--- out/Assets/Scripts/Player/player_move.lua
+++ out/Assets/Scripts/Player/player_move.lua
@@ -1,30 +1,43 @@
+local velocity = 30
+
+THIS.speedX = 10.0
+THIS.speedY = THIS.speedX * 0.4
+THIS.flipX = false
+
 function THIS:onUpdate(dt)
-	local velocity = 9*dt*60
-	if(Input.isKept(Input.Key_Up)) then
-		self:move(0, 0, -velocity / 1.2)
-	end
-	if(Input.isKept(Input.Key_Left)) then
-		self:move(-velocity, 0, 0)
-	end
-	if(Input.isKept(Input.Key_Down)) then
-		self:move(0, 0, velocity / 1.2)
-	end
-	if(Input.isKept(Input.Key_Right)) then
-		self:move(velocity, 0, 0)
+
+	local arm = self:armature()
+
+	self.dirX = 0
+	self.dirY = 0
+
+	if (Input.isKept(Input.Key_Up)) then
+		self.dirY = -1
+	elseif (Input.isKept(Input.Key_Down)) then
+		self.dirY = 1
 	end
 
-	local armature = self:armature()
-
-	if (Input.isPressed(Input.Key_Left)) then
-		armature:fadeIn("Run_051", 0.25)
-		armature:setFlipX(true)
-	elseif (Input.isPressed(Input.Key_Right)) then
-		armature:fadeIn("Run_051", 0.25)
-		armature:setFlipX(false)
-	elseif (Input.isPressed(Input.Key_Up)) then
-		armature:fadeIn("Walk_029", 0.25)
-	elseif (Input.isPressed(Input.Key_Down)) then
-		armature:fadeIn("Walk_029", 0.25)
+	if (Input.isKept(Input.Key_Left)) then
+		self.dirX = -1
+		self.flipX = true
+	elseif (Input.isKept(Input.Key_Right)) then
+		self.dirX = 1
+		self.flipX = false
 	end
 
+	if (self.dirX ~= 0 or self.dirY ~= 0) then
+		self.angle = Math.atan2(self.dirY, self.dirX)
+
+		self:move(Math.cos(self.angle) * self.speedX * dt * 60, 0, Math.sin(self.angle) * self.speedY * dt * 60)
+	end
+
+	arm:setFlipX(self.flipX)
+
+	if (self.dirX ~= 0) then
+		arm:fadeIn("Cedmin_Run_051", 0.25)
+	elseif (self.dirY ~= 0) then
+		arm:fadeIn("Cedmin_Walk_029", 0.25)
+	else
+		arm:fadeIn("Cedmin_Idle_008", 0.25)
+	end
 end
