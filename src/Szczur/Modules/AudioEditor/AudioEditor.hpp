e3a5643a04c43d3cd9f1ab9a203c598781371240
Mapet <mapet1206@gmail.com>
2018-06-21 11:07:57 +0200

    Separated MusicEditor from AudioEditor

--- src/Szczur/Modules/AudioEditor/AudioEditor.hpp
+++ src/Szczur/Modules/AudioEditor/AudioEditor.hpp
@@ -1,60 +1,45 @@
 #pragma once
+
 #include "Szczur/Utility/Modules/Module.hpp"
 #include "Szczur/Modules/Window/Window.hpp"
 #include "Szczur/Modules/Input/Input.hpp"
 #include "Szczur/Modules/Sound/SoundManager.hpp"
 #include "Szczur/Modules/Music/Music.hpp"
-#include "SoundEditor.hpp"
 
+#include "SoundEditor.hpp"
+#include "MusicEditor.hpp"
 
 #include <ImGui/imgui.h>
 #include <ImGui/imgui-SFML.h>
+
 #include <fstream>
+
 namespace rat
 {
-    struct PlaylistHolder 
-    {
-        std::vector<std::string> playlistsNames;
-        
-        Music* music;
-
-        void savePlaylists();
-        void loadPlaylist();
-        void addMusic(const std::string& playlistName);
-
-        void addPlaylist(const std::string& name) {
-            playlistsNames.push_back(name);
-            music->addPlaylist(name, {});  
-        };
-    };
-
     class AudioEditor : public Module <Window, Input, Music>
     {
-        const int BUFFER=256;      
         SoundEditor _soundEditor;
-        bool deleteField=false;
-        char file[256]={};
-        char soundName[256]={};
-        char saveFileName[256]={};
-        char loadFileName[256]={};
+        MusicEditor _musicEditor;
 
-        bool _showSoundEditor{false};
-        bool _showMusicEditor{false};
+        const int BUFFER = 256;  
 
-        bool _addingPlaylist{false};
-        bool _addingEffect{false};
+        bool deleteField = false;
 
-        PlaylistHolder _playlistHolder;
-        MusicAssets* musicAssets;
+        char file[256]          = {};
+        char soundName[256]     = {};
+        char saveFileName[256]  = {};
+        char loadFileName[256]  = {};
 
-        std::string currentEditingMusicFile = "";
-        std::string currentPlaylist = "";
+        bool _showSoundEditor {false};
+        bool _showMusicEditor {false};
 
     public:
+
         AudioEditor();
+
         void render();
 
-        void addSound(const std::string& soundName,const std::string& soundFileName);
+        void addSound(const std::string& soundName, const std::string& soundFileName);
         
         void save(const std::string& fileName);
         void load(const std::string& fileName);
