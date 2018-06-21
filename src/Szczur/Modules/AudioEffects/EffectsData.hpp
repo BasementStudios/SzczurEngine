7702c88ddcce4093bf4837b220c1ea2e37e8c4d1
Mapet <mapet1206@gmail.com>
2018-06-21 11:44:17 +0200

    Fixed warnings

--- src/Szczur/Modules/AudioEffects/EffectsData.hpp
+++ src/Szczur/Modules/AudioEffects/EffectsData.hpp
@@ -20,16 +20,16 @@ namespace rat
         }
 
         void reset() {
-            lowCutoff             = 200;
-            lowGain               = 1;
-            highCutoff            = 6000;
-            highGain              = 1;
-            lowMidCenter          = 500;
-            lowMidWidth           = 1;
-            lowMidGain            = 1;
-            highMidCenter         = 3000;
-            highMidWidth          = 1;
-            highMidGain           = 1;
+            lowCutoff             = 200.f;
+            lowGain               = 1.f;
+            highCutoff            = 6000.f;
+            highGain              = 1.f;
+            lowMidCenter          = 500.f;
+            lowMidWidth           = 1.f;
+            lowMidGain            = 1.f;
+            highMidCenter         = 3000.f;
+            highMidWidth          = 1.f;
+            highMidGain           = 1.f;
         }
     };
 
@@ -54,19 +54,19 @@ namespace rat
         }
 
         void reset() {
-            density               = 1;
-            diffusion             = 1;
-            gain                  = 0.32;
-            gainHf                = 0.89;
-            decayTime             = 1.49;
-            decayHfRatio          = 0.83;
-            reflectionsGain       = 0.05;
-            reflectionsDelay      = 0.007;
-            lateReverbGain        = 1.26;
-            lateReverbDelay       = 0.011;
-            airAbsorptionGainHf   = 0.994;
-            roomRolloffFactor     = 0;
-            decayHfLimit           = true;
+            density               = 1.f;
+            diffusion             = 1.f;
+            gain                  = 0.32f;
+            gainHf                = 0.89f;
+            decayTime             = 1.49f;
+            decayHfRatio          = 0.83f;
+            reflectionsGain       = 0.05f;
+            reflectionsDelay      = 0.007f;
+            lateReverbGain        = 1.26f;
+            lateReverbDelay       = 0.011f;
+            airAbsorptionGainHf   = 0.994f;
+            roomRolloffFactor     = 0.f;
+            decayHfLimit          = true;
         }
     };
 
@@ -83,11 +83,11 @@ namespace rat
         }
 
         void reset() {
-            delay                 = 0.1;
-            lrDelay               = 0.1;
-            damping               = 0.5;
-            feedback              = 0.5;
-            spread                = -1;
+            delay                 = 0.1f;
+            lrDelay               = 0.1f;
+            damping               = 0.5f;
+            feedback              = 0.5f;
+            spread                = -1.f;
         }
     };
 }
