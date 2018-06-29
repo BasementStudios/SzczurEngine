7aec5109c8dcd66b9ec6e51e1766da2ba26d6027
Piotr Krupa <piotrkrupa06@gmail.com>
2018-06-29 18:47:19 +0200

    Probably fixed problem with removing armature data

--- src/Szczur/Modules/World/Data/ArmatureDisplayData.hpp
+++ src/Szczur/Modules/World/Data/ArmatureDisplayData.hpp
@@ -5,28 +5,28 @@
 #include "Szczur/Modules/DragonBones/SF3DArmatureDisplay.hpp"
  
 namespace rat
+{ 
+
+class ArmatureDisplayData
 {
- 
-    class ArmatureDisplayData
-    {
-    private:
-        sf3d::Texture* _texture;
+private:
+    sf3d::Texture* _texture;
 
-        std::string _name;
-        std::string _folderPath;
+    std::string _name;
+    std::string _folderPath;
     
-    protected:
-        constexpr static auto _assetsFolderPath = "";
-        constexpr static auto _skeFilePath = "/armature_ske.json";
-        constexpr static auto _textureAtlasFilePath = "/armature_tex.json";
-        constexpr static auto _textureFilePath = "/armature_tex.png";
+protected:
+    constexpr static auto _assetsFolderPath = "";
+    constexpr static auto _skeFilePath = "/armature_ske.json";
+    constexpr static auto _textureAtlasFilePath = "/armature_tex.json";
+    constexpr static auto _textureFilePath = "/armature_tex.png";
 
-    public:
-        ArmatureDisplayData(const std::string& path);
-        ~ArmatureDisplayData();
+public:
+    ArmatureDisplayData(const std::string& path);
+    ~ArmatureDisplayData();
 
-        const auto& getName() { return _name; }
-        const auto& getFolderPath() { return _folderPath; }
-    };
+    const auto& getName() { return _name; }
+    const auto& getFolderPath() { return _folderPath; }
+};
  
 }
