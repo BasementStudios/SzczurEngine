85f38538063e780db4ca8a65c9b087ec53e33307
Knayder <knayder@outlook.com>
2018-07-05 00:43:27 +0200

    Added 'create' to RenderTexture

--- src/Szczur/Utility/SFML3D/RenderTexture.cpp
+++ src/Szczur/Utility/SFML3D/RenderTexture.cpp
@@ -3,34 +3,43 @@
 #include <iostream>
 
 namespace sf3d {
-	RenderTexture::RenderTexture(ShaderProgram* program, const glm::uvec2 & size, float FOV) :
-	RenderTarget(size, FOV, program) {
-		glGenFramebuffers(1, &_FBO);
-		glGenRenderbuffers(1, &_RBO);
+	RenderTexture::RenderTexture() {
 
-		//Framebuffer
-		glBindFramebuffer(GL_FRAMEBUFFER, _FBO);
-		//Texture
-			_texture.create(size);
-			glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, _texture.getID(), 0);
-
-			//RenderBuffer
-			glBindRenderbuffer(GL_RENDERBUFFER, _RBO);
-				glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, size.x, size.y);
-			glBindRenderbuffer(GL_RENDERBUFFER, 0);
-			glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, _RBO);
-
-			if(glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
-				std::cout << "FRAMEBUFFER CREATION ERROR\n";
-
-		glBindFramebuffer(GL_FRAMEBUFFER, 0);
-		//Framebuffer
+	}
+	RenderTexture::RenderTexture(ShaderProgram* program, const glm::uvec2 & size, float FOV)
+	{
+		create(program, size, FOV);
 	}
 	RenderTexture::~RenderTexture() {
 		if(_FBO)
 			glDeleteFramebuffers(1, &_FBO);
 		if(_RBO)
 			glDeleteRenderbuffers(1, &_RBO);
+	}
+
+	void RenderTexture::create(ShaderProgram * program, const glm::uvec2 & size, float FOV) {
+		RenderTarget::create(size, FOV, program);
+
+		glGenFramebuffers(1, &_FBO);
+		glGenRenderbuffers(1, &_RBO);
+
+		//Framebuffer
+		glBindFramebuffer(GL_FRAMEBUFFER, _FBO);
+		//Texture
+		_texture.create(size);
+		glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, _texture.getID(), 0);
+
+		//RenderBuffer
+		glBindRenderbuffer(GL_RENDERBUFFER, _RBO);
+		glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, size.x, size.y);
+		glBindRenderbuffer(GL_RENDERBUFFER, 0);
+		glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, _RBO);
+
+		if(glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
+			std::cout << "FRAMEBUFFER CREATION ERROR\n";
+
+		glBindFramebuffer(GL_FRAMEBUFFER, 0);
+		//Framebuffer
 	}
 
 	Texture* RenderTexture::getTexture() {
--- src/Szczur/Utility/SFML3D/RenderTexture.hpp
+++ src/Szczur/Utility/SFML3D/RenderTexture.hpp
@@ -8,8 +8,12 @@
 namespace sf3d {
 	class RenderTexture : public RenderTarget {
 	public:
+		RenderTexture();
 		RenderTexture(ShaderProgram* program, const glm::uvec2& size, float FOV = 45.f);
 		~RenderTexture();
+
+		void create(ShaderProgram* program, const glm::uvec2& size, float FOV = 45.f);
+
 		Texture* getTexture();
 	private:
 		GLuint _FBO{0u};
