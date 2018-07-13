6d38001c91d6d597f3d03a6ae33580b29162b21e
MetGang <patrick_stritch@wp.pl>
2018-07-13 04:16:13 +0200

    Updated glm serializers

--- src/Szczur/Utility/Convert/GLMStreams.hpp
+++ src/Szczur/Utility/Convert/GLMStreams.hpp
@@ -1,11 +1,98 @@
 #pragma once
 
-#include <ostream>
+#include <iostream>
 
-#include <glm/vec3.hpp>
+#include <nlohmann/json.hpp>
 
-inline std::ostream& operator<< (std::ostream& stream, glm::vec3 const& value)
+#include <glm/detail/type_vec.hpp>
+#include <glm/detail/type_mat.hpp>
+
+namespace detail
 {
-	stream << "{x: " << value.x << ", y:" << value.y << ", z:" << value.z << "}";
-	return stream;
+
+///
+template <glm::length_t L, typename T, glm::qualifier Q, size_t... Ns>
+std::ostream& OstreamSerialize(std::ostream& out, const glm::vec<L, T, Q>& vec, std::index_sequence<Ns...>)
+{
+    out << "{ " << vec[0]; ((out << ", " << vec[Ns + 1]), ...); out << " }";
+
+    return out;
+}
+
+}
+
+///
+template <glm::length_t L, typename T, glm::qualifier Q>
+std::ostream& operator << (std::ostream& out, const glm::vec<L, T, Q>& vec)
+{
+    return detail::OstreamSerialize(out, vec, std::make_index_sequence<L - 1>{});
+}
+
+namespace nlohmann
+{
+
+///
+template <glm::length_t L, typename T, glm::qualifier Q>
+struct adl_serializer<glm::vec<L, T, Q>>
+{
+    ///
+    template <size_t... Ns>
+    static void Serialize(nlohmann::json& j, const glm::vec<L, T, Q>& value, std::index_sequence<Ns...>)
+    {
+        ((j[Ns] = value[Ns]), ...);
+    }
+
+    ///
+    static void to_json(nlohmann::json& j, const glm::vec<L, T, Q>& value)
+    {
+        Serialize(j, value, std::make_index_sequence<L>{});
+    }
+
+    ///
+    template <size_t... Ns>
+    static void Deserialize(const nlohmann::json& j, glm::vec<L, T, Q>& value, std::index_sequence<Ns...>)
+    {
+        ((value[Ns] = j[Ns]), ...);
+    }
+
+    ///
+    static void from_json(const nlohmann::json& j, glm::vec<L, T, Q>& value)
+    {
+        Deserialize(j, value, std::make_index_sequence<L>{});
+    }
+
+};
+
+///
+template <glm::length_t C, glm::length_t R, typename T, glm::qualifier Q>
+struct adl_serializer<glm::mat<C, R, T, Q>>
+{
+    ///
+    template <size_t... Ns>
+    static void Serialize(nlohmann::json& j, const glm::mat<C, R, T, Q>& value, std::index_sequence<Ns...>)
+    {
+        ((j[Ns] = value[Ns]), ...);
+    }
+
+    ///
+    static void to_json(nlohmann::json& j, const glm::mat<C, R, T, Q>& value)
+    {
+        Serialize(j, value, std::make_index_sequence<C>{});
+    }
+
+    ///
+    template <size_t... Ns>
+    static void Deserialize(const nlohmann::json& j, glm::mat<C, R, T, Q>& value, std::index_sequence<Ns...>)
+    {
+        ((value[Ns] = j[Ns]), ...);
+    }
+
+    ///
+    static void from_json(const nlohmann::json& j, glm::mat<C, R, T, Q>& value)
+    {
+        Deserialize(j, value, std::make_index_sequence<C>{});
+    }
+
+};
+
 }
