352e8e758d77669a9139ea37d315a7b4987632d6
Janix4000 <janix4000@gmail.com>
2018-07-15 13:25:11 +0200

    Some tests

--- out/Assets/Dialog/Config/dialogConfig.lua
+++ out/Assets/Dialog/Config/dialogConfig.lua
@@ -9,8 +9,7 @@ function dialogGUI:onInit()
  
     self.interface = GUI.addInterface()
     self.dialogText = self.interface:addTextArea()
-    self.listHolder = self.interface:addWidget()
-    self.list = self.listHolder:addList()
+    self.list = self.interface:addList()
 end
  
 function dialogGUI:reset()
@@ -18,15 +17,12 @@ function dialogGUI:reset()
     local dT = self.dialogText
     dT:setPropPosition(0.5, 1)
     dT:setFont(GUI.getFont("Assets/Dialog/config/BKANT.TTF"))
-    dT:setCharacterPropSize(0.02)
+    dT:setCharacterPropSize(0.028)
     dT:setToCenterAlign()
-   
-    local lH = self.listHolder
-    lH:setPropPosition(0, 0.5)
-    lH:setPropSize(0, 0.3)
+    dT:setPropSize(1, 0.0)
    
     local list = self.list
-    list:setPropBetweenPad(0.02)
+    list:setPropBetweenPad(0.025)
     list:setPropPosition(0.3, 0.5)
     list:makeStaticPropPositing()
     list:setPropOrigin(0.0, 1.0)
@@ -40,11 +36,13 @@ function dialogGUI:getButton(caption, r, g, b, a, iconId)
     local button = self.list:addImage()
     button:setTexture(GUI.getTexture("Assets/Dialog/config/button.png"))
     button:setColor(50, 50, 50, 0)
-    button:setPropSize(0.7, 0.03)
+
+    button:setPropSize(0.4, 0.04)
+    button:setPropPadding(0.17, 0.0)
+
     button:makeChildrenUnresizable()
     button:makeChildrenPenetrable()
     button:makeChildrenUncolorable()
-    button:setPropOrigin(0.1, 0.0)
    
    
     local text = button:addText()
