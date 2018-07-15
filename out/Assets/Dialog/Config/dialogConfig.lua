8377985582c285f173240d711e3580e58eef2f1c
Janix4000 <janix4000@gmail.com>
2018-07-15 18:27:42 +0200

    Better GUI?
    Do uzgodnienia z Metriko

--- out/Assets/Dialog/Config/dialogConfig.lua
+++ out/Assets/Dialog/Config/dialogConfig.lua
@@ -17,13 +17,16 @@ function dialogGUI:reset()
     local dT = self.dialogText
     dT:setPropPosition(0.5, 1)
     dT:setFont(GUI.getFont("Assets/Dialog/config/BKANT.TTF"))
-    dT:setCharacterPropSize(0.028)
+    dT:setCharacterPropSize(0.03)
     dT:setToCenterAlign()
     dT:setPropSize(1, 0.0)
+    dT:setOutlinePropThickness(0.03)
+    dT:setOutlineColor(255, 0, 255, 255)
+    dT:setColor(200, 0, 200, 255)
    
     local list = self.list
     list:setPropBetweenPad(0.025)
-    list:setPropPosition(0.3, 0.5)
+    list:setPropPosition(0.1, 0.75)
     list:makeStaticPropPositing()
     list:setPropOrigin(0.0, 1.0)
 end
@@ -37,20 +40,28 @@ function dialogGUI:getButton(caption, r, g, b, a, iconId)
     button:setTexture(GUI.getTexture("Assets/Dialog/config/button.png"))
     button:setColor(50, 50, 50, 0)
 
-    button:setPropSize(0.4, 0.04)
+    button:setPropSize(0.4, 0.05)
     button:setPropPadding(0.17, 0.0)
 
     button:makeChildrenUnresizable()
     button:makeChildrenPenetrable()
     button:makeChildrenUncolorable()
+    button:setFullSizeFilling()
    
    
     local text = button:addText()
     text:setFont(GUI.getFont("Assets/Dialog/config/BKANT.TTF"))
-    text:setCharacterPropSize(0.03)
+    text:setCharacterPropSize(0.035)
     text:setPropPosition(0.0, 0.5)
     text:setColor(r, g, b, a)
     text:setString(caption)
+    text:setOutlinePropThickness(0.03)
+    text:setOutlineColor(
+        ((r+50 < 255) and r+50 or 255),
+        ((g+50 < 255) and g+50 or 255),
+        ((b+50 < 255) and b+50 or 255),
+        255
+    )
    
     function button:onHoverOut()
         self:setColor(255, 255, 255, 0)
