f4619fc2bbd852096841f3890462b1e2c825b395
PsychoXIVI <psychoxivi@gmail.com>
2018-07-17 23:55:44 +0200

    Minor change in `Sprite`

--- src/Szczur/Utility/SFML3D/Sprite.cpp
+++ src/Szczur/Utility/SFML3D/Sprite.cpp
@@ -14,26 +14,18 @@ void Sprite::setTexture(const Texture& texture)
 	_texture = &texture;
 
 	const glm::vec2 size = texture.getSize();
-	_vertices[0] = {
-		{0.f, 0.f, 0.f},
-		{1.f, 1.f, 1.f, 1.f},
-		{0.f, 0.f}
-	};
-	_vertices[1] = {
-		{size.x, 0.f, 0.f},
-		{1.f, 1.f, 1.f, 1.f},
-		{1.f, 0.f}
-	};
-	_vertices[2] = {
-		{size.x, -size.y, 0.f},
-		{1.f, 1.f, 1.f, 1.f},
-		{1.f, 1.f}
-	};
-	_vertices[3] = {
-		{0.f, -size.y, 0.f},
-		{1.f, 1.f, 1.f, 1.f},
-		{0.f, 1.f}
-	};
+
+	_vertices[0].position = {0.f, 0.f, 0.f};
+	_vertices[0].texCoord = {0.f, 0.f};
+
+	_vertices[1].position = {size.x, 0.f, 0.f};
+	_vertices[1].texCoord = {1.f, 0.f};
+
+	_vertices[2].position = {size.x, -size.y, 0.f};
+	_vertices[2].texCoord = {1.f, 1.f};
+	
+	_vertices[3].position = {0.f, -size.y, 0.f};
+	_vertices[3].texCoord = {0.f, 1.f};
 }
 
 
