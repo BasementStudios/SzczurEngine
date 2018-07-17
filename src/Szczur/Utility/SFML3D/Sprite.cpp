bb7a41f1d96c9a47f25affb0cc55a9d18b059420
PsychoXIVI <psychoxivi@gmail.com>
2018-07-17 21:14:14 +0200

    Added `Sprite` construction from `Texture`
    And `setTexture` from reference instead of pointer

--- src/Szczur/Utility/SFML3D/Sprite.cpp
+++ src/Szczur/Utility/SFML3D/Sprite.cpp
@@ -1,43 +1,62 @@
 #include "Sprite.hpp"
 
+#include <glm/vec2.hpp>
+
 #include "RenderTarget.hpp"
 #include "Texture.hpp"
 
-namespace sf3d {
-	Sprite::Sprite() {
+namespace sf3d
+{
 
-	}
+/* Properties */
+void Sprite::setTexture(const Texture& texture)
+{
+	_texture = &texture;
 
-	void Sprite::setTexture(Texture* texture) {
-		if(texture) {
-			_texture = texture;
-			auto size = texture->getSize();
-			_vertices[0] = {
-				{0.f, 0.f, 0.f},
-				{1.f, 1.f, 1.f, 1.f},
-				{0.f, 0.f}
-			};
-			_vertices[1] = {
-				{(float)size.x, 0.f, 0.f},
-				{1.f, 1.f, 1.f, 1.f},
-				{1.f, 0.f}
-			};
-			_vertices[2] = {
-				{(float)size.x, -(float)size.y, 0.f},
-				{1.f, 1.f, 1.f, 1.f},
-				{1.f, 1.f}
-			};
-			_vertices[3] = {
-				{0.f, -(float)size.y, 0.f},
-				{1.f, 1.f, 1.f, 1.f},
-				{0.f, 1.f}
-			};
-		}
-	}
+	const glm::vec2 size = texture.getSize();
+	_vertices[0] = {
+		{0.f, 0.f, 0.f},
+		{1.f, 1.f, 1.f, 1.f},
+		{0.f, 0.f}
+	};
+	_vertices[1] = {
+		{size.x, 0.f, 0.f},
+		{1.f, 1.f, 1.f, 1.f},
+		{1.f, 0.f}
+	};
+	_vertices[2] = {
+		{size.x, -size.y, 0.f},
+		{1.f, 1.f, 1.f, 1.f},
+		{1.f, 1.f}
+	};
+	_vertices[3] = {
+		{0.f, -size.y, 0.f},
+		{1.f, 1.f, 1.f, 1.f},
+		{0.f, 1.f}
+	};
+}
 
-	void Sprite::draw(RenderTarget & target, RenderStates states) const {
-		states.transform *= getTransform();
-		states.texture = _texture;
-		target.draw(_vertices, states);
-	}
+
+
+/* Operators */
+Sprite::Sprite()
+{
+	;
+}
+
+Sprite::Sprite(const Texture& texture) 
+{
+	setTexture(texture);
+}
+
+
+
+/* Methods */
+void Sprite::draw(RenderTarget & target, RenderStates states) const
+{
+	states.transform *= getTransform();
+	states.texture = _texture;
+	target.draw(_vertices, states);
+}
+
 }
