e26c1ecfacf8ffe7478797795f52c698b56c6a66
PsychoXIVI <psychoxivi@gmail.com>
2018-07-18 19:39:02 +0200

    Separated GLM conversion (Streams and JSON)

--- src/Szczur/Utility/Convert/GLMStreams.hpp
+++ src/Szczur/Utility/Convert/GLMStreams.hpp
@@ -2,11 +2,9 @@
 
 #include <iostream>
 
-#include <nlohmann/json.hpp>
-
 #include <glm/detail/type_vec.hpp>
 #include <glm/detail/type_mat.hpp>
-#include <glm/matrix.hpp>
+#include <glm/matrix.hpp> // transpose
 
 namespace detail
 {
@@ -51,71 +49,4 @@ std::ostream& operator << (std::ostream& out, const glm::mat<C, R, T, Q>& mat)
     return detail::OstreamSerialize(out, mat, std::make_index_sequence<C - 1>{});
 }
 
-namespace nlohmann
-{
 
-///
-template <glm::length_t L, typename T, glm::qualifier Q>
-struct adl_serializer<glm::vec<L, T, Q>>
-{
-    ///
-    template <size_t... Ns>
-    static void Serialize(nlohmann::json& j, const glm::vec<L, T, Q>& value, std::index_sequence<Ns...>)
-    {
-        ((j[Ns] = value[Ns]), ...);
-    }
-
-    ///
-    static void to_json(nlohmann::json& j, const glm::vec<L, T, Q>& value)
-    {
-        Serialize(j, value, std::make_index_sequence<L>{});
-    }
-
-    ///
-    template <size_t... Ns>
-    static void Deserialize(const nlohmann::json& j, glm::vec<L, T, Q>& value, std::index_sequence<Ns...>)
-    {
-        ((value[Ns] = j[Ns]), ...);
-    }
-
-    ///
-    static void from_json(const nlohmann::json& j, glm::vec<L, T, Q>& value)
-    {
-        Deserialize(j, value, std::make_index_sequence<L>{});
-    }
-
-};
-
-///
-template <glm::length_t C, glm::length_t R, typename T, glm::qualifier Q>
-struct adl_serializer<glm::mat<C, R, T, Q>>
-{
-    ///
-    template <size_t... Ns>
-    static void Serialize(nlohmann::json& j, const glm::mat<C, R, T, Q>& value, std::index_sequence<Ns...>)
-    {
-        ((j[Ns] = value[Ns]), ...);
-    }
-
-    ///
-    static void to_json(nlohmann::json& j, const glm::mat<C, R, T, Q>& value)
-    {
-        Serialize(j, value, std::make_index_sequence<C>{});
-    }
-
-    ///
-    template <size_t... Ns>
-    static void Deserialize(const nlohmann::json& j, glm::mat<C, R, T, Q>& value, std::index_sequence<Ns...>)
-    {
-        ((value[Ns] = j[Ns]), ...);
-    }
-
-    ///
-    static void from_json(const nlohmann::json& j, glm::mat<C, R, T, Q>& value)
-    {
-        Deserialize(j, value, std::make_index_sequence<C>{});
-    }
-
-};
-
-}
