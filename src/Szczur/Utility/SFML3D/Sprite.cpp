ccc6db220e2da0958ea3dd9d15999df1db4f63dc
MetRiko <18maximus8050@gmail.com>
2018-07-19 03:29:34 +0200

    Added setTextureRect in Sprite

--- src/Szczur/Utility/SFML3D/Sprite.cpp
+++ src/Szczur/Utility/SFML3D/Sprite.cpp
@@ -35,6 +35,37 @@ namespace sf3d {
 		}
 	}
 
+    void Sprite::setTextureRect(glm::vec2 position, glm::vec2 size) {
+        if(_texture) {
+            glm::vec2 texSize = _texture->getSize();
+            if(texSize.x > 0 && texSize.y > 0) {
+                _vertices[0] = {
+                    {0.f, 0.f, 0.f},
+                    {1.f, 1.f, 1.f, 1.f},
+                    {position.x / texSize.x, position.y / texSize.y}
+                };
+                _vertices[1] = {
+                    {size.x, 0.f, 0.f},
+                    {1.f, 1.f, 1.f, 1.f},
+                    {(position.x + size.x) / texSize.x, position.y / texSize.y}
+                    //{1.f, 0.f}
+                };
+                _vertices[2] = {
+                    {size.x, -size.y, 0.f},
+                    {1.f, 1.f, 1.f, 1.f},
+                    {(position.x + size.x) / texSize.x, (position.y + size.y) / texSize.y}
+                    //{1.f, 1.f}
+                };
+                _vertices[3] = {
+                    {0.f, -size.y, 0.f},
+                    {1.f, 1.f, 1.f, 1.f},
+                    {position.x / texSize.x, (position.y + size.y) / texSize.y}
+                    //{0.f, 1.f}
+                };
+            }
+        }
+    }
+
 	void Sprite::draw(RenderTarget & target, RenderStates states) const {
 		states.transform *= getTransform();
 		states.texture = _texture;
--- src/Szczur/Utility/SFML3D/Sprite.hpp
+++ src/Szczur/Utility/SFML3D/Sprite.hpp
@@ -5,19 +5,21 @@
 #include "VertexArray.hpp"
 
 namespace sf3d {
-	class RenderTarget;
-	class Texture;
-	class RenderStates;
+    class RenderTarget;
+    class Texture;
+    class RenderStates;
 
-	class Sprite : public Transformable, public Drawable {
-	public:
-		Sprite();
+    class Sprite : public Transformable, public Drawable {
+    public:
+        Sprite();
 
-		void setTexture(Texture* texture);
+        void setTexture(Texture* texture);
 
-		virtual void draw(RenderTarget& target, RenderStates states) const override;
-	private:
-		Texture* _texture{nullptr};
-		VertexArray _vertices{4u};
-	};
+        void setTextureRect(glm::vec2 position, glm::vec2 size);
+
+        virtual void draw(RenderTarget& target, RenderStates states) const override;
+    private:
+        Texture* _texture{nullptr};
+        VertexArray _vertices{4u};
+    };
 }
