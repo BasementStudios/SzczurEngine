2732639e4dc154cc4b5ac355d5d35b8222fa49e6
MetRiko <18maximus8050@gmail.com>
2018-07-19 03:57:16 +0200

    Fix for ArmatureDisplayData

--- src/Szczur/Modules/World/Data/ArmatureDisplayData.cpp
+++ src/Szczur/Modules/World/Data/ArmatureDisplayData.cpp
@@ -3,6 +3,11 @@
 #include <experimental/filesystem>
  
 #include "Szczur/Modules/DragonBones/SF3DFactory.hpp"
+#include "Szczur/Utility/Logger.hpp"
+
+#include "Szczur/Modules/FileSystem/DragDrop.hpp"
+
+using std::chrono::system_clock;
 
 namespace rat
 {
@@ -12,22 +17,69 @@ ArmatureDisplayData::ArmatureDisplayData(const std::string& path)
 {
     _name = std::experimental::filesystem::path(path).filename().string();
 
+    checkForReload();
+    load();
+}
+    
+ArmatureDisplayData::~ArmatureDisplayData() 
+{
+    unload();
+}
+
+void ArmatureDisplayData::load()
+{
+    LOG_INFO("Loading armature data: '", _name, "'...");
+
     auto dbFactory = dragonBones::SF3DFactory::get();
 
     _texture = new sf3d::Texture;
     _texture->loadFromFile(_folderPath + _textureFilePath);
 
-    dbFactory->loadDragonBonesData(_folderPath + _skeFilePath, _name);
+    dbFactory->loadDragonBonesData(_folderPath  + _skeFilePath, _name);
     dbFactory->loadTextureAtlasData(_folderPath + _textureAtlasFilePath, _texture, _name);
 }
-    
-ArmatureDisplayData::~ArmatureDisplayData() {
+
+void ArmatureDisplayData::unload()
+{
+    LOG_INFO("Removing armature data: '", _name, "'...");
+
     auto dbFactory = dragonBones::SF3DFactory::get();
 
     dbFactory->removeDragonBonesData(_name);
     dbFactory->removeTextureAtlasData(_name);
     delete _texture;
 }
- 
+
+void ArmatureDisplayData::reload()
+{
+    checkForReload();
+
+    if (_needReload)
+    {
+        unload();
+        load();
+
+        _needReload = false;
+    }
+}
+
+void ArmatureDisplayData::checkForReload()
+{
+    namespace fs = std::experimental::filesystem;
+
+    auto lastSkeFileUpdate          = system_clock::to_time_t(fs::last_write_time(_folderPath + _skeFilePath));
+    auto lastTextureAtlasFileUpdate = system_clock::to_time_t(fs::last_write_time(_folderPath + _textureAtlasFilePath));
+    auto lastTextureFileUpdate      = system_clock::to_time_t(fs::last_write_time(_folderPath + _textureFilePath));
+
+    if (lastSkeFileUpdate           != _lastSkeFileUpdate ||
+        lastTextureAtlasFileUpdate  != _lastTextureAtlasFileUpdate ||
+        lastTextureFileUpdate       != _lastTextureFileUpdate)
+    {
+        _lastSkeFileUpdate = lastSkeFileUpdate;
+        _lastTextureAtlasFileUpdate = lastTextureAtlasFileUpdate;
+        _lastTextureFileUpdate = lastTextureFileUpdate;
+        _needReload = true;
+    }
 }
  
+}
--- src/Szczur/Modules/World/Data/ArmatureDisplayData.hpp
+++ src/Szczur/Modules/World/Data/ArmatureDisplayData.hpp
@@ -1,5 +1,7 @@
 #pragma once
  
+#include <ctime>
+
 #include "Szczur/Utility/SFML3D/Drawable.hpp"
  
 #include "Szczur/Modules/DragonBones/SF3DArmatureDisplay.hpp"
@@ -14,7 +16,12 @@ private:
 
     std::string _name;
     std::string _folderPath;
-    
+
+    bool _needReload = false;
+    std::time_t _lastSkeFileUpdate = 0;
+    std::time_t _lastTextureAtlasFileUpdate = 0;
+    std::time_t _lastTextureFileUpdate = 0;
+
 protected:
     constexpr static auto _assetsFolderPath = "";
     constexpr static auto _skeFilePath = "/armature_ske.json";
@@ -24,6 +31,12 @@ protected:
 public:
     ArmatureDisplayData(const std::string& path);
     ~ArmatureDisplayData();
+
+    void load();
+    void unload();
+    void reload();
+
+    void checkForReload();
 
     const auto& getName() { return _name; }
     const auto& getFolderPath() { return _folderPath; }
